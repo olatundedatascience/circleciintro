733d6735c1d33d4e9b37fed0008f63c1d7e1a99e
Raimi.Aliu <Raimi.Aliu@FCMB-IT-L16582>
2020-07-07 14:00:37 +0100

    added second test and functions

--- app.test.js
+++ app.test.js
@@ -1,20 +1,36 @@
-const ranges = require("./functions")
+const {ranges, arrayToJson} = require("./functions")
 
 
 describe("testing functions for circle ci", ()=> {
     it('should not be null', ()=>{
-        const items = ranges.ranges(2, 10, 3)
+        const items = ranges(2, 10, 3)
 
         expect(items).toBeDefined()
         expect(items.length).toEqual(3)
     }) 
 
     it("should be part of returned data", ()=>{
-        const items = ranges.ranges(0,100,5)
+        const items = ranges(0,100,5)
         
         expect(items.length).toBeGreaterThan(5)
         expect(items[1]).toEqual(5)
     })
 // this run the test and test it...
     
+})
+
+describe("testing function that convert array to json", ()=>{
+    it('should return viable json', ()=>{
+        const jsonObject = arrayToJson([{name:"taye", location:"NG"}, {name:"Adu", location:"GH"}])
+
+        expect(typeof(jsonObject)).toEqual("object")
+        expect(jsonObject["code"]).toEqual("00")
+    })
+
+    it('should throw and return 99 as code meaning failure', ()=>{
+        const jsonObject = arrayToJson(231)
+
+        //expect(typeof(jsonObject)).toEqual("object")
+        expect(jsonObject["code"]).toEqual("99")
+    })
 })
--- functions.js
+++ functions.js
@@ -6,6 +6,42 @@ const ranges = (start, stop, step =2) => {
 
     return items;
 }
-module.exports = {
-    ranges:ranges
+
+const arrayToJson = (array=[]) => {
+    const getType = typeof(array[0])
+    let returnedObject = {}
+   
+    try {
+        if(Array.isArray(array)) {
+            returnedObject["data"] = array
+            returnedObject["message"]= `elementSize: ${array.length}`
+            returnedObject["code"] = "00"
+            return returnedObject
+        }
+        else {
+            returnedObject["data"] = 'element is not an array'
+            returnedObject["message"]= `null`
+            returnedObject["code"] = "99"
+        
+            return returnedObject
+        }
+           
+    }
+    catch(er) {
+            returnedObject["data"] = er
+            returnedObject["message"]= `null`
+            returnedObject["code"] = "99"
+        
+            return returnedObject
+    }
+   
 }
+
+//console.log(arrayToJson([0,1,2,3]))
+
+
+module.exports = {
+    ranges:ranges,
+    arrayToJson:arrayToJson
+}
+
